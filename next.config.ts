b6281cffa499d8f166469a2030a403e2bb0633f5
Mohim129 <countrysidefarm15@gmail.com>
2026-07-23 01:46:57 +0600

    recommendation ai updated

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
+    contentDispositionType: "inline",
     remotePatterns: [
       {
         protocol: "https",
